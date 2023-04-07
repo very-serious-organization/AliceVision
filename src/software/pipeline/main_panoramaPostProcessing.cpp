d3ffed299c4c7871ba22c8da71bfe876da6394fb
demoulinv <vincent.demoulin@technicolor.com>
2023-04-07 15:54:01 +0200

    [IOs] Updates io.cpp and io.h Add new input options compressionMethod and compressionLevel in imageProcessing and panoramaPostProcessing.

--- src/software/pipeline/main_panoramaPostProcessing.cpp
+++ src/software/pipeline/main_panoramaPostProcessing.cpp
@@ -194,6 +194,8 @@ int aliceVision_main(int argc, char** argv)
     std::string inputPanoramaPath;
     std::string outputPanoramaPath;
     image::EStorageDataType storageDataType = image::EStorageDataType::Float;
+    image::EImageExrCompression compressionMethod = image::EImageExrCompression::Auto;
+    int compressionLevel = 0;
     image::EImageColorSpace outputColorSpace = image::EImageColorSpace::LINEAR;
     const size_t maxProcessingSize = 2000;
     bool fillHoles = false;  
@@ -208,6 +210,14 @@ int aliceVision_main(int argc, char** argv)
     po::options_description optionalParams("Optional parameters");
     optionalParams.add_options()
         ("storageDataType", po::value<image::EStorageDataType>(&storageDataType)->default_value(storageDataType), ("Storage data type: " + image::EStorageDataType_informations()).c_str())
+
+        ("compressionMethod", po::value<image::EImageExrCompression>(&compressionMethod)->default_value(compressionMethod),
+         ("Compression Method: " + image::EImageExrCompression_informations()).c_str())
+
+        ("compressionLevel", po::value<int>(&compressionLevel)->default_value(compressionLevel),
+         "Compression Level (must be strictly positive to be considered)\n"
+         "Only dwaa, dwab, zip and zips compression methods are concerned.")
+
         ("fillHoles", po::value<bool>(&fillHoles)->default_value(fillHoles), "Execute fill holes algorithm")
         ("outputColorSpace", po::value<image::EImageColorSpace>(&outputColorSpace)->default_value(outputColorSpace), "Color space for the output panorama.");
 
@@ -271,7 +281,30 @@ int aliceVision_main(int argc, char** argv)
     oiio::ImageSpec outputSpec(inputSpec);
     outputSpec.tile_width = 0;
     outputSpec.tile_height = 0;
-	outputSpec.attribute("compression", "zip");
+
+    std::string compressionMethod_str = "none";
+
+    if (compressionMethod == image::EImageExrCompression::Auto)
+    {
+        compressionMethod_str = "zip";
+    }
+    else if (compressionMethod != image::EImageExrCompression::None)
+    {
+        compressionMethod_str = EImageExrCompression_enumToString(compressionMethod);
+        if (compressionLevel > 0)
+        {
+            if ((compressionMethod == image::EImageExrCompression::DWAA || compressionMethod == image::EImageExrCompression::DWAB))
+            {
+                compressionMethod_str += ":" + std::to_string(compressionLevel);
+            }
+            else if ((compressionMethod == image::EImageExrCompression::ZIP || compressionMethod == image::EImageExrCompression::ZIPS))
+            {
+                compressionMethod_str += ":" + std::to_string(std::min<int>(compressionLevel, 9));
+            }
+        }
+    }
+	outputSpec.attribute("compression", compressionMethod_str);
+
     outputSpec.extra_attribs.remove("openexr:lineOrder");
     outputSpec.attribute("AliceVision:ColorSpace",image::EImageColorSpace_enumToString(outputColorSpace));
 
